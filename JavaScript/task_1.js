1c8b82fc018983e50a039101b374c91eaedb7fd3
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-08-26 16:13:38 +0100

    html and css task

---
+++ JavaScript/task_1.js
@@ -0,0 +1,18 @@
+//THE FOLLOWING LINES OF CODE DISPLAYS MY FULL NAME.
+const name = "Qozeem Odeniran"; //I used the dataypt "const" here because my name can't be changed.
+console.log(name);//prints my name to the console.
+
+//THE FOLLOWING LINES OF CODE SAVED THE LIST OF COURSES IN AN ARRAY.
+var courses = ["HTML", "CSS", "JavaScript", "C-SHARP", "PHP", "NODE-JS", "Java"];//saving my courses in an array
+console.log(courses);
+
+//THE FOLLOWING LINES OF CODE, AFTER SATISFYING CERTAIN CONDITIONS,PRINTS ODD NUMBERS BETWEEN 1 AND 200 (BOTH ENDS INCLUSIVE)
+count = 0;
+count = courses.lenght;
+for(count = 1; count <= 200; count++) {
+	if (count % 2 != 0) {
+		console.log(count);
+	}
+}
+//THIS IS THE END OF MY CODE!!!
+
